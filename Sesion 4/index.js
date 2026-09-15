c0553b65e41947a17f49dc64a4258022cabf79f0
Edwin Paredes <paredesvasqueze@iestptrujillo.net>
2026-09-15 18:50:36 -0500

    semana 4

---
+++ Sesion 4/index.js
@@ -0,0 +1,21 @@
+/*
+fetch("https://randomuser.me/api/")
+.then(response => response.json())
+.then(data => console.log(data))
+*/
+
+setTimeout(
+() => {
+    console.log("Holitas");
+    }
+    ,
+    1000
+);
+
+console.log("Holitas 2");
+let j = 1;
+for (let i = 1; i<=2000000000; i++)
+{
+j=i;
+}
+
